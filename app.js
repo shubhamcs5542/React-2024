64776b8409c8882958e3b8b2a7667ff1d9750140
shubham sawant <shubham.sawant5542@gmail.com>
2024-01-02 12:30:32 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1",{id: "heading"},"Hello Shubham!!! How are you");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
